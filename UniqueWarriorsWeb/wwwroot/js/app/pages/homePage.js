8610fc5d2680a764688219060a5ebb0ddc8c376c
Deadly Artist <ocram.siebenstein@web.de>
2025-02-18 21:53:22 +0100

    fix

--- UniqueWarriorsWeb/wwwroot/js/app/pages/homePage.js
+++ UniqueWarriorsWeb/wwwroot/js/app/pages/homePage.js
@@ -2,8 +2,8 @@ class HomePageManager extends PageManager {
 
     load() {
         let element = fromHTML(`<div class="listVertical">`);
-        element.appendChild(fromHTML(`<h1 class="brand-text">Welcome to Cyberfantasy: Unique Warriors`));
-        let link = fromHTML(`<a><h2>A Cyberfantasy TTRPG created for epic combats in a mix of magical High Fantasy and interdimensional SciFi.`);
+        element.appendChild(fromHTML(`<h1 class="brand-text centeredText">Welcome to Cyberfantasy: Unique Warriors`));
+        let link = fromHTML(`<a><h2 class="centeredText">A Cyberfantasy TTRPG created for epic combats in a mix of dystopian Cyberpunk, magical High Fantasy, and interdimensional SciFi.`);
         element.appendChild(link);
         link.setAttribute('href', "/app/rules");
 
